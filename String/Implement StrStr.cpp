535490b76f9ad8421cdfe5e6ceecc265f954429b
Sonali Agrawal <soagraw@microsoft.com>
2019-06-23 13:32:26 +0530

    String search

---
+++ String/Implement StrStr.cpp
@@ -0,0 +1,14 @@
+int Solution::strStr(const string &haystack, const string &needle) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    int l = needle.size();
+    int l2 = haystack.size();
+    
+    for(int beg = 0; beg <= l2-l; beg++)
+    {
+        
+    }
+}
